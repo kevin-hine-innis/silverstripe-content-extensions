ad2b31a714001328fc5991432a0c84072da128cd
kevin-hine-innis <83420315+kevin-hine-innis@users.noreply.github.com>
2021-09-15 19:17:54 -0400

    Update IMExtendedPage.php
    Don't add additional fields to error pages.

--- src/IMExtendedPage.php
+++ src/IMExtendedPage.php
@@ -2,6 +2,7 @@
 
 namespace InnisMaggiore\SilverstripeContentExtensions;
 
+use SilverStripe\ErrorPage\ErrorPage;
 use Silverstripe\ORM\DataExtension;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
@@ -20,7 +21,7 @@ class IMExtendedPage extends DataExtension
     public function updateCMSFields(FieldList $fields) {
         $owner = $this->getOwner();
 
-        if (is_subclass_of($owner, SiteTree::class) && !is_a($owner, RedirectorPage::class)) {
+        if (is_subclass_of($owner, SiteTree::class) && !is_a($owner, RedirectorPage::class) && !is_a($owner, ErrorPage::class)) {
             $fields->addFieldToTab("Root.Main", TextField::create("H1", "H1"), "Content");
             $fields->addFieldToTab('Root.Main', TextField::create('PageTitle', 'Page Title'), 'MetaDescription');
             // this isn't plugged into a template. For reference for content folks
